81ac9dd797fb0aadfbb89015158498e54670bff8
root <root@hbase001-master-0.jcu.edu.au>
2013-05-03 05:07:59 +1000

    Update manifest errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,22 +2,23 @@
 
 class swoop($tomcat_ram = 1024)
 {
+	require swoop::params
 	file { $swoop::params::war_directory:
 		ensure => "directory",
-		alias => "Swoop-dir",
+		alias => "swoop-dir",
 	}
 	file {"${swoop::params::war_directory}/Swoop.war" :
 		ensure  => present,
 		source  => "puppet:///modules/swoop/dist/Swoop-WAR-${swoop::params::version}.war",
 		alias   => "swoop-war",
-		require => "swoop-dir"
+		require => File["swoop-dir"]
 	}
 	file{"${swoop::params::context_directory}/Swoop.xml":
 		alias   => "context-file",
-		content => template("context/swoop.xml.erb"),
+		content => template("swoop/context/swoop.xml.erb"),
 	}
 	file_line{"tomcat_ram":
 		path => $swoop::params::config_file,
-		line => "JAVA_OPTS=\${JAVA_OPTS} -Xmx ${tomcat_ram}"
+		line => "JAVA_OPTS=\"\${JAVA_OPTS} -Xmx${tomcat_ram}\""
 	}
 }
