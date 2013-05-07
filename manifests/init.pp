320cbbcabc1e3535740418cb727e45e8694932b3
nigelb <nigel.blair@gmail.com>
2013-05-08 09:41:12 +1000

    Unpack war and link to hadoop/hbase config

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,11 +7,35 @@ class swoop($tomcat_ram = "1024M")
 		ensure => "directory",
 		alias => "swoop-dir",
 	}
-	file {"${swoop::params::war_directory}/Swoop.war" :
+	file {"/root/Swoop.war" :
 		ensure  => present,
 		source  => "puppet:///modules/swoop/dist/Swoop-WAR-${swoop::params::version}.war",
 		alias   => "swoop-war",
 		require => File["swoop-dir"]
+	}
+	exec{"unzip /root/Swoop.war":
+		command  => "unzip /root/Swoop.war",
+		cwd      => $swoop::params::war_directory,
+		subscribe => File["swoop-war"],
+		refreshonly => true,
+		alias    => "swoop-unzip-war"
+		user     => $swoop::params::servlet_user,
+		group    => $swoop::params::servlet_group,
+		path     => "/usr/bin"
+        }
+	file{"$swoop::params::war_directory/WEB-INF/classes/hbase-site.xml":
+		target  => $swoop::params::hbase_site,
+		ensure  => link,
+		user    => $swoop::params::servlet_user,
+		group   => $swoop::params::servlet_group,
+		require => Exec["swoop-unzip-war"],
+	}
+	file{"$swoop::params::war_directory/WEB-INF/classes/core-site.xml":
+		target  => $swoop::params::hadoop_site,
+		ensure  => link,
+		user    => $swoop::params::servlet_user,
+		group   => $swoop::params::servlet_group,
+		require => Exec["swoop-unzip-war"],
 	}
 	file{"${swoop::params::context_directory}/ROOT.xml":
 		alias   => "context-file",
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,10 @@ class swoop::params (
 	$version = "0.0.1-SNAPSHOT",
 	$war_directory = "/usr/share/swoop",
 	$context_directory = "/etc/tomcat6/Catalina/localhost",
-	$config_file = "/etc/tomcat6/tomcat6.conf"
+	$config_file = "/etc/tomcat6/tomcat6.conf",
+	$hbase_site = "/opt/hbase/hbase/conf/hbase-site.xml",
+	$hadoop_site = "/opt/hadoop/hadoop/conf/core-site.xml",
+	$servler_user = "tomcat",
+	$servlet_group = "tomcat"
 		
 ){}
