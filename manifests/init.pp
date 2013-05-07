dd98ef3e27ce69fb9235199b3dfa7867f486a2ce
nigelb <nigel.blair@gmail.com>
2013-05-07 14:43:18 +1000

    Fix tomcat ram

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # /etc/puppet/modules/hadoop/manifests/init.pp
 
-class swoop($tomcat_ram = 1024)
+class swoop($tomcat_ram = "1024M")
 {
 	file { $swoop::params::war_directory:
 		ensure => "directory",
@@ -18,6 +18,6 @@ class swoop($tomcat_ram = 1024)
 	}
 	file_line{"tomcat_ram":
 		path => $swoop::params::config_file,
-		line => "JAVA_OPTS=\${JAVA_OPTS} -Xmx ${tomcat_ram}"
+		line => "JAVA_OPTS=\${JAVA_OPTS} -Xmx${tomcat_ram} -Xms${tomcat_ram}"
 	}
 }
