6489ebe98f0d21ea51feacb7d1283b2406183870
nigelb <nigel.blair@gmail.com>
2013-05-20 10:24:23 +1000

    Add /usr/bin to path

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -17,6 +17,6 @@ class swoop::cli(
 		creates  => "${cli_directory}/bin",
 		require  => File["swoop-cli-dir"],
 		alias    => "swoop-cli-untar",
-		path     => "/bin"
+		path     => ["/bin", "/usr/bin"]
         }
 }
